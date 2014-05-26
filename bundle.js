8ca5aa47ff272fc75ea05d20445203a844913a21
dhoko <aurelien@procheo.fr>
2014-05-26 14:12:04 +0200

    Update build

--- bundle.js
+++ bundle.js
@@ -51,11 +51,10 @@ module.exports = ['babelfish', '$timeout', function (babelfish, $timeout) {
  * Translate your application
  */
 module.exports = angular.module('servalI18n', [])
-    .factory('babelfish', require('./services/babelfish'))
+    .provider('babelfish', require('./providers/babelfish'))
     .directive('i18nLoad', require('./directives/i18nLoad'))
     .directive('i18nBind', require('./directives/i18nBind'))
     .filter('translate', require('./filters/translate'))
-    .value("custom")
     .run(['babelfish', '$state','$rootScope', function(babelfish, $state, $rootScope) {
 
         // Update the translation when you change a page
@@ -65,13 +64,13 @@ module.exports = angular.module('servalI18n', [])
 
         babelfish.load();
     }]);
-},{"./directives/i18nBind":1,"./directives/i18nLoad":2,"./filters/translate":3,"./services/babelfish":5}],5:[function(require,module,exports){
+},{"./directives/i18nBind":1,"./directives/i18nLoad":2,"./filters/translate":3,"./providers/babelfish":5}],5:[function(require,module,exports){
 /**
- * I18n Service
+ * I18n Service Provider
  * Load your translations and update $rootScope
  * It gives you access to your translation.
  */
-module.exports = ['$rootScope', '$http','custom', function($rootScope, $http, custom) {
+module.exports = function() {
 
     var i18n = {
         current: "",
@@ -99,64 +98,6 @@ module.exports = ['$rootScope', '$http','custom', function($rootScope, $http, cu
         ],
         current: ""
     };
-
-    angular.extend(config, custom);
-
-
-    /**
-     * Load a translation to the $scope
-     * - doc BCP 47 {@link http://tools.ietf.org/html/bcp47}
-     * - doc Value of HTML5 lang attr {@link http://webmasters.stackexchange.com/questions/28307/value-of-the-html5-lang-attribute}
-     * @param {String} lang Your language cf BCP 47
-     */
-    function setTranslation(page) {
-
-        page = page || config.state;
-        i18n.currentState = page;
-        lang = i18n.current;
-        i18n.active = true;
-
-        if(i18n.data[lang]) {
-            angular.extend(i18n.data[lang]['_common'], {languages: i18n.available});
-            angular.extend(i18n.data[lang][page], i18n.data[lang]['_common']);
-
-            if(config.namespace) {
-                $rootScope[config.namespace] = i18n.data[lang][page];
-            }else {
-                angular.extend($rootScope, i18n.data[lang][page]);
-            }
-        }
-    }
-
-    /**
-     * Load a translation to the $scope for a language
-     * - doc BCP 47 {@link http://tools.ietf.org/html/bcp47}
-     * - doc Value of HTML5 lang attr {@link http://webmasters.stackexchange.com/questions/28307/value-of-the-html5-lang-attribute}
-     * @trigger {Event} i18n:babelfish:changed {previous:XXX,value:XXX2}
-     * @param {String} lang Your language cf BCP 47
-     */
-    function loadLanguage(lang) {
-
-        var old = document.documentElement.lang;
-        if(!lang) {
-            lang =  (old + '-' + old.toUpperCase()) || 'en-EN';
-        }else {
-            document.documentElement.lang = lang.split('-')[0];
-        }
-
-        config.lang = i18n.current = lang;
-
-        $rootScope.$emit('i18n:babelfish:changed', {
-            previous: (old + '-' + old.toUpperCase()),
-            value: lang
-        });
-
-        // Load the new language if we do not already have it
-        if(config.lazy && !i18n.data[lang]) {
-            service.load();
-        }
-
-    }
 
     /**
      * Load your url from lazy mode
@@ -187,63 +128,134 @@ module.exports = ['$rootScope', '$http','custom', function($rootScope, $http, cu
         i18n.data[i18n.current] = data;
     }
 
-    // Listen when you change the language in your application
-    $rootScope.$on('i18n:babelfish:changed', function() {
-        setTranslation(i18n.currentState);
-    });
+    /**
+     * Configure the service with a provider from the config of your module
+     * @param  {Object} params Configuration object
+     * @return {void}
+     */
+    this.languages = function languagesConfig(params) {
+        angular.extend(config, params);
+    }
 
-    var service = {
+    /**
+     * Babelfish service
+     */
+    this.$get = ['$rootScope', '$http', function($rootScope, $http) {
 
-        load: function load(url, name) {
+        /**
+         * Load a translation to the $scope
+         * - doc BCP 47 {@link http://tools.ietf.org/html/bcp47}
+         * - doc Value of HTML5 lang attr {@link http://webmasters.stackexchange.com/questions/28307/value-of-the-html5-lang-attribute}
+         * @param {String} lang Your language cf BCP 47
+         */
+        function setTranslation(page) {
 
-            url = url || loadLazyDefaultUrl();
+            page = page || config.state;
+            i18n.currentState = page;
+            lang = i18n.current;
+            i18n.active = true;
 
-            var lang = config.lang || document.documentElement.lang + '-' + document.documentElement.lang.toUpperCase();
+            if(i18n.data[lang]) {
+                angular.extend(i18n.data[lang]['_common'], {languages: i18n.available});
+                angular.extend(i18n.data[lang][page], i18n.data[lang]['_common']);
 
-            if(i18n.data[i18n.current]) {
-                return;
+                if(config.namespace) {
+                    $rootScope[config.namespace] = i18n.data[lang][page];
+                }else {
+                    angular.extend($rootScope, i18n.data[lang][page]);
+                }
             }
-            return $http.get(url)
-                .error(function() {
-                    alert("Cannot load i18n translation file");
-                })
-                .success(function (data) {
-
-                    if(config.lazy) {
-                        config.current = name;
-                    }
-                    i18n.current = lang;
-                    buildI18n(data);
-
-                    if(config.lazy) {
-                        i18n.available.push(i18n.current);
-                    }else {
-                        i18n.available = Object.keys(i18n.data);
-                    }
-                })
-                .then(function() {
-                    setTranslation();
-                });
-        },
-
-        get: function get(lang) {
-            return angular.extend(i18n.data[lang || i18n.current][i18n.currentState] , i18n.data[lang || i18n.current]['_common']);
-        },
-
-        all: function all(lang) {
-            return i18n.data[lang || i18n.current];
-        },
-
-        current: function current() {
-            return i18n.current;
-        },
-        updateState: setTranslation,
-        updateLang: loadLanguage,
-        isLoaded: function isLoaded() {
-            return i18n.active;
         }
-    };
 
-    return service;
-}];
+        /**
+         * Load a translation to the $scope for a language
+         * - doc BCP 47 {@link http://tools.ietf.org/html/bcp47}
+         * - doc Value of HTML5 lang attr {@link http://webmasters.stackexchange.com/questions/28307/value-of-the-html5-lang-attribute}
+         * @trigger {Event} i18n:babelfish:changed {previous:XXX,value:XXX2}
+         * @param {String} lang Your language cf BCP 47
+         */
+        function loadLanguage(lang) {
+
+            var old = document.documentElement.lang;
+            if(!lang) {
+                lang =  (old + '-' + old.toUpperCase()) || 'en-EN';
+            }else {
+                document.documentElement.lang = lang.split('-')[0];
+            }
+
+            config.lang = i18n.current = lang;
+
+            $rootScope.$emit('i18n:babelfish:changed', {
+                previous: (old + '-' + old.toUpperCase()),
+                value: lang
+            });
+
+            // Load the new language if we do not already have it
+            if(config.lazy && !i18n.data[lang]) {
+                service.load();
+            }
+
+        }
+
+        // Listen when you change the language in your application
+        $rootScope.$on('i18n:babelfish:changed', function() {
+            setTranslation(i18n.currentState);
+        });
+
+        var service = {
+
+            load: function load(url, name) {
+
+                url = url || loadLazyDefaultUrl();
+
+                var lang = config.lang || document.documentElement.lang + '-' + document.documentElement.lang.toUpperCase();
+
+                if(i18n.data[i18n.current]) {
+                    return;
+                }
+                return $http.get(url)
+                    .error(function() {
+                        alert("Cannot load i18n translation file");
+                    })
+                    .success(function (data) {
+
+                        if(config.lazy) {
+                            config.current = name;
+                        }
+                        i18n.current = lang;
+                        buildI18n(data);
+
+                        if(config.lazy) {
+                            i18n.available.push(i18n.current);
+                        }else {
+                            i18n.available = Object.keys(i18n.data);
+                        }
+                    })
+                    .then(function() {
+                        setTranslation();
+                    });
+            },
+
+            get: function get(lang) {
+                return angular.extend(i18n.data[lang || i18n.current][i18n.currentState] , i18n.data[lang || i18n.current]['_common']);
+            },
+
+            all: function all(lang) {
+                return i18n.data[lang || i18n.current];
+            },
+
+            current: function current() {
+                return i18n.current;
+            },
+            updateState: setTranslation,
+            updateLang: loadLanguage,
+            isLoaded: function isLoaded() {
+                return i18n.active;
+            }
+        };
+
+        return service;
+    }];
+
+};
 },{}]},{},[4])
